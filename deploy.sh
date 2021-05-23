f27efb350e889e3d2eb03eac92790aff41d748ad
David Steiner <david_j_steiner@yahoo.co.nz>
2021-05-23 23:29:48 +0100

    Exit on failed build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cargo build --release --target x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl || exit
 cd infrastructure || exit
-cp ../target/x86_64-unknown-linux-musl/release/experimental_authoriser ./bootstrap && zip lambda.zip ./bootstrap && rm ./bootstrap
+cp ../target/x86_64-unknown-linux-musl/release/lambda-authoriser ./bootstrap && zip lambda.zip ./bootstrap && rm ./bootstrap
 cdk deploy
